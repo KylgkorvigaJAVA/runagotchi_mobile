44a7fadcab290eeb42f7f886087ca213b8879d07
Kaspar <kylgkorvigajava@gmail.com>
2026-05-20 16:21:09 +0300

    added expo-navigation-bar to hide system nav buttons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
+import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Hide navigation bar with immersive mode (shows on swipe-up)
+    NavigationBar.setVisibilityAsync("hidden");
+  }, []);
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
